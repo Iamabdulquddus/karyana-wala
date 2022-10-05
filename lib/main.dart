b027b53cfd95b71c0781099e82f808cb90823a04
Abdul Quddus <abdulquddus7782@gmail.com>
2022-10-05 22:21:09 +0500

    abq

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:karyana_wala/providers/auth_provider.dart';
+import 'package:provider/provider.dart';
 import 'config/routes.dart';
 import 'constants/colors.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  // setPathUrlStrategy();
-  runApp(const KaryanaWala());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => AuthProvider(),
+        ),
+      ],
+      child: KaryanaWala(),
+    ),
+  );
 }
 
 class KaryanaWala extends StatelessWidget {
@@ -18,7 +32,6 @@ class KaryanaWala extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Palette.myColors,
         visualDensity: VisualDensity.adaptivePlatformDensity,
-
       ),
       initialRoute: MyRoutes.getSplashRoute(),
       getPages: MyRoutes.appRoutes(),
---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,103 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:karyana_wala/services/user_services.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../config/routes.dart';
+
+class AuthProvider  with ChangeNotifier{
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  late String smsOtp, verificationId;
+  String error = '';
+  final UserServices _userServices = UserServices();
+
+  Future<void> verifyPhone (BuildContext context , String number) async {
+
+    verificationCompleted(PhoneAuthCredential credential) async {
+      await _auth.signInWithCredential(credential);
+    }
+
+    verificationFailed(FirebaseAuthException e) {
+      print(e.code);
+    }
+
+    final PhoneCodeSent smsOtpSend = (String verId, int resendToken) async {
+      verificationId = verId;
+
+      smsOtpDialog(context, number);
+
+    } as PhoneCodeSent;
+
+    try{
+      _auth.verifyPhoneNumber(
+        phoneNumber: number,
+          verificationCompleted: verificationCompleted,
+          verificationFailed: verificationFailed,
+          codeSent: smsOtpSend,
+          codeAutoRetrievalTimeout: (String verId){
+          verificationId = verId;
+          }
+      );
+
+    }catch(e){
+      print(e);
+
+    }
+  }
+
+  Future smsOtpDialog(BuildContext context, String number){
+    return showDialog(
+      context: context,
+      builder: (BuildContext context){
+        return AlertDialog(
+          title: Column(
+            children: const [
+              Text('Verify Code'),
+              SizedBox(height: 6,),
+              Text('Enter 6 digit OTP recieved as sms'),
+            ],
+          ),
+          content: Container(
+            height: 85,
+            child: TextField(textAlign: TextAlign.center, keyboardType: TextInputType.number, maxLength: 6, onChanged: (value){
+              smsOtp = value;
+            },),
+          ),
+          actions: [
+            TextButton(onPressed: () async {
+              try {
+                PhoneAuthCredential credential = PhoneAuthProvider.credential(verificationId: verificationId, smsCode: smsOtp);
+
+            final User? user =     (await _auth.signInWithCredential(credential)).user;
+            
+            _createUser(id: user!.uid, number:  user.phoneNumber);
+
+            if(user != null ){
+              Get.back();
+              Get.offAllNamed(MyRoutes.getHomeRoute());
+            }else {
+              print('login failed');
+            }
+
+              }catch (e){
+                error = 'invalid otp';
+                print (e.toString());
+                Get.back();
+              }
+            }, child: const Text('Done'))
+          ],
+        );
+      }
+    );
+  }
+
+  void _createUser({required String id,  String? number}){
+    _userServices.createUserData({
+      "id" : id,
+      "number" :number,
+    });
+  }
+
+}
